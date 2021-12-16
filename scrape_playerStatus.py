71d2cb7c79527c6f1a318b4faa52d4d4f2c3e90a
jmatthews <jmatthews@deloitte.co.uk>
2021-12-16 17:23:21 +0000

    funtionslised in get_player_status()

--- scrape_playerStatus.py
+++ scrape_playerStatus.py
@@ -5,15 +5,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from io import StringIO
 
-URL = "https://www.fplanalytics.com/playerStatus.html"
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+def get_player_status():
+    URL = "https://www.fplanalytics.com/playerStatus.html"
 
-divs = soup.findAll('div', attrs={'class':'container'})
-s = str(divs[1].find('input'))
-data_url = s.split('=')[2].split(';')[0].split("'")[1]
-data = requests.get(data_url)
-csv_data = StringIO(data.text)
-df = pd.read_csv(csv_data)
-print(df.head())
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    divs = soup.findAll('div', attrs={'class':'container'})
+    s = str(divs[1].find('input'))
+    data_url = s.split('=')[2].split(';')[0].split("'")[1]
+    data = requests.get(data_url)
+    csv_data = StringIO(data.text)
+    return csv_data
+    # df = pd.read_csv(csv_data)
+    # print(df.head())
